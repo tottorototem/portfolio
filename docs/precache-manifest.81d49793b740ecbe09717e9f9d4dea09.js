700cc167bb2cf20d444ae24bce423461eda7c986
Spencer Rudnick <spencerudnick@gmail.com>
2019-06-01 15:52:02 +0200

    build for initial deployment

---
+++ docs/precache-manifest.81d49793b740ecbe09717e9f9d4dea09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6bc44cd8614effa72707f9ce44bd03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24be79d20d5dd1e5b9e",
+    "url": "/static/css/main.43e6b92d.chunk.css"
+  },
+  {
+    "revision": "925b051b488f680e09f2",
+    "url": "/static/js/2.2626d23a.chunk.js"
+  },
+  {
+    "revision": "d24be79d20d5dd1e5b9e",
+    "url": "/static/js/main.4ee233b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
